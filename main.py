aeb01f1b612be8d057ac869339cdaa99c0b9811c
pxir029 <pxtopir@atomicmail.io>
2026-09-02 14:00:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1409,6 +1409,3 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=PORT, log_level="info", workers=1)
-
-
-
